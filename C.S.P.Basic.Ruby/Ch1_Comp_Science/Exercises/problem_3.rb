901f8a4fef41117fe3fcb1f061f0734aa4e2e2ef
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-07-30 18:33:23 -0400

    Fixes

--- C.S.P.Basic.Ruby/Ch1_Comp_Science/Exercises/problem_3.rb
+++ C.S.P.Basic.Ruby/Ch1_Comp_Science/Exercises/problem_3.rb
@@ -1,70 +1,7 @@
-# #3)
+#3)
 
 
-#     # A palindrom program.
-# #----------------------This is how i started the problem. -----------------------
-# ####This does not have regexp. So, as long as you input letters with spaces (no characters), it should work perfectly.
-
-
-
-#     puts "Please enter a sentence or word: "
-#     input = gets.chomp
-#     downcase_input = input.downcase
-
-
-#     letter = downcase_input.delete " "
-#     input_no_spaces = letter.chars
-#     #this variable is the first half of the input after it deletes spaces.
-#     first_half_input = input_no_spaces.first(input_no_spaces.count/2)
-#     #this variable is the other half of the input, need to revese it to match the first_half_input
-#     second_half_input = input_no_spaces.last(input_no_spaces.count/2).reverse
-
-
-#      if first_half_input == second_half_input
-#         puts "#{input}' is a Palindrom"
-#     else
-#         puts "#{input} is not Palindrom"
-#      end
-
-
-
-# #---------    This is a better program to play with, you can enter any word or sentence, and It will tell you if it's a Palindrom
-
-
-
-
-    # puts "Please enter a sentence or word: "
-    # input = gets.chomp
-
-    #     # I needed Regexp to be able to remove every character to be able to compare the string.
-    #     #This rusult variable store thie Regexp that  allows every none-letter-characters, spaces, digits.
-    # result = Regexp.new('([\d\s\W])')
-    #     #Downcase method is needed so I can compare only downcase letters. In other words, upcase (A) & downcase (a) are not equal.
-    # to_downcase_input = input.downcase
-
-    #     #gsub method allowed me to pass two paramerers, Regexp and substitute with empty string, I coud also put a space between the string and use "delete_space" variable to be able to delete spaces. (see below)
-    #     #---- delete_spaces = remove_none_letters.delete " "
-    # remove_none_letters = to_downcase_input.gsub(result,"")
-
-    #     #after i remove non-letter-characters, I used chars to get an array back
-    # each_letter = remove_none_letters.chars
-
-    #     #This method is the key. I counted the array collection of indexes, in this case, letters. I devided by 2, and I used the "first" method to get the first half of the array
-    # first_half_input = each_letter.first(each_letter.count/2)
-    #     #this variable is the other half of the array, I needed to revese it to match the first_half_input.
-    # second_half_input = each_letter.last(each_letter.count/2).reverse
-
-    #     #this conditional simply compares the input method, if it matches then is a Palindrom. Else, is not!!!!
-
-    #  if first_half_input == second_half_input
-    #     puts "#{input}' is a Palindrom"
-    # else
-    #     puts "#{input} is not Palindrom"
-    #  end
-
-
-#------------ I broke this into functions, it is the same as the code above.
-
+# A palindrom program.
 
 
 def welcome
@@ -121,3 +58,67 @@ welcome
 puts
 game
 
+
+
+# #----------------------This is how i started the problem. -----------------------
+# ####This does not have regexp. So, as long as you input letters with spaces (no characters), it should work perfectly.
+
+
+
+    puts "Please enter a sentence or word: "
+    input = gets.chomp
+    downcase_input = input.downcase
+
+
+    letter = downcase_input.delete " "
+    input_no_spaces = letter.chars
+    #this variable is the first half of the input after it deletes spaces.
+    first_half_input = input_no_spaces.first(input_no_spaces.count/2)
+    #this variable is the other half of the input, need to revese it to match the first_half_input
+    second_half_input = input_no_spaces.last(input_no_spaces.count/2).reverse
+
+
+     if first_half_input == second_half_input
+        puts "#{input}' is a Palindrom"
+    else
+        puts "#{input} is not Palindrom"
+     end
+
+
+
+# #---------    This is a better program to play with, you can enter any word or sentence, and It will tell you if it's a Palindrom
+
+
+
+
+    puts "Please enter a sentence or word: "
+    input = gets.chomp
+
+        # I needed Regexp to be able to remove every character to be able to compare the string.
+        #This rusult variable store thie Regexp that  allows every none-letter-characters, spaces, digits.
+    result = Regexp.new('([\d\s\W])')
+        #Downcase method is needed so I can compare only downcase letters. In other words, upcase (A) & downcase (a) are not equal.
+    to_downcase_input = input.downcase
+
+        #gsub method allowed me to pass two paramerers, Regexp and substitute with empty string, I coud also put a space between the string and use "delete_space" variable to be able to delete spaces. (see below)
+        #---- delete_spaces = remove_none_letters.delete " "
+    remove_none_letters = to_downcase_input.gsub(result,"")
+
+        #after i remove non-letter-characters, I used chars to get an array back
+    each_letter = remove_none_letters.chars
+
+        #This method is the key. I counted the array collection of indexes, in this case, letters. I devided by 2, and I used the "first" method to get the first half of the array
+    first_half_input = each_letter.first(each_letter.count/2)
+        #this variable is the other half of the array, I needed to revese it to match the first_half_input.
+    second_half_input = each_letter.last(each_letter.count/2).reverse
+
+        #this conditional simply compares the input method, if it matches then is a Palindrom. Else, is not!!!!
+
+     if first_half_input == second_half_input
+        puts "#{input}' is a Palindrom"
+    else
+        puts "#{input} is not Palindrom"
+     end
+
+
+#------------ I broke this into functions, it is the same as the code above.
