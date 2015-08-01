46d893e4a1dcb53b55f9b5eac1a3b322e0db7355
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-08-01 13:41:17 -0400

    Wrote some steps to start working on this problem

--- C.S.P.Basic.Ruby/Ch1_Comp_Science/Exercises/problem_4.rb
+++ C.S.P.Basic.Ruby/Ch1_Comp_Science/Exercises/problem_4.rb
@@ -1,3 +1,20 @@
+#------- IN PROGRESS  ------
+
+# Craft an 3 different algorithm pretending each is a maze with different sizes and difficulty.
+
+#1st maze
+
+# Step 1 - Think of the problem.
+=begin
+
+  -maze consist in going the other side, however if it gets to a path that is blocked, it has to go
+    back.
+        - I can play with true-false for each move
+        -I can count moves, and for every false, I can deduct points.
+        -Do ASCII Art
+=end
+
+
 up = true || false
 down = true || false
 right = true || false
@@ -6,7 +23,7 @@ left = true || false
 move_start = 0
 moves += 1
 
-puts "Your move"
+puts "Start moving "
 move = gets.chomp
 
 #legal moves
